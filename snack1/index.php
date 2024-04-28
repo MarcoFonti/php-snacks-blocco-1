7817313bd9d1930eb473b3f4244798213c104e5e
MarcoFonti <theemporiumg@gmail.com>
2024-04-28 14:07:23 +0200

    add commit

--- snack1/index.php
+++ snack1/index.php
@@ -1,54 +1,56 @@
-<?php 
-    
+<?php
+
+
+/* CREO ARRAY DI ARRAY ASSOCIATIVI */
 $array_matches = [
     [
-        'home_team' =>[
-            'name'=> 'Virtus Segafredo Bologna',
+        'home_team' => [
+            'name' => 'Virtus Segafredo Bologna',
             'score' => '65'
         ],
-            
-        'out_team'=> [
-            'name'=> 'Givova Scafati Basket',
+
+        'out_team' => [
+            'name' => 'Givova Scafati Basket',
             'score' => '75'
         ]
     ],
-        
+
     [
-        'home_team' =>[
-            'name'=> 'Vanoli Basket Cremona',
+        'home_team' => [
+            'name' => 'Vanoli Basket Cremona',
             'score' => '85'
         ],
 
-        'out_team'=> [
-            'name'=> 'Generazione Vincente Napoli Basket',
+        'out_team' => [
+            'name' => 'Generazione Vincente Napoli Basket',
             'score' => '100'
         ]
     ],
-        
+
     [
-        'home_team' =>[
-            'name'=> 'Dolomiti Energia Trentino',
+        'home_team' => [
+            'name' => 'Dolomiti Energia Trentino',
             'score' => '90'
         ],
-            
-        'out_team'=> [
-            'name'=> 'NutriBullet Treviso Basket',
+
+        'out_team' => [
+            'name' => 'NutriBullet Treviso Basket',
             'score' => '87'
         ]
-            
+
     ],
 
     [
-        'home_team' =>[
-            'name'=> 'EA7 Emporio Armani Milano',
+        'home_team' => [
+            'name' => 'EA7 Emporio Armani Milano',
             'score' => '115'
         ],
-        
-        'out_team'=> [
-            'name'=> 'Happy Casa Brindisi',
+
+        'out_team' => [
+            'name' => 'Happy Casa Brindisi',
             'score' => '112'
         ]
-            
+
     ],
 
 ];
@@ -58,39 +60,44 @@ $array_matches = [
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SNACK 1</title>
     <!-- BOOTSTRAP -->
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/css/bootstrap.min.css' integrity='sha512-b2QcS5SsA8tZodcDtGRELiGv5SaKSk1vDHDaQRda0htPYWZ6046lr3kJ5bAAQdpV2mmA/4v0wQF9MyU6/pDIAg==' crossorigin='anonymous'/>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/css/bootstrap.min.css'
+        integrity='sha512-b2QcS5SsA8tZodcDtGRELiGv5SaKSk1vDHDaQRda0htPYWZ6046lr3kJ5bAAQdpV2mmA/4v0wQF9MyU6/pDIAg=='
+        crossorigin='anonymous' />
     <!-- CSS -->
     <link rel="stylesheet" href="./css/style.css">
 </head>
+
 <body>
     <div class="container mt-3">
         <h1 class="text-center text-danger">PARTITE</h1>
         <ul>
-        <!-- CICLO SU ARRAY PRICIPALE -->
-        <?php foreach($array_matches as $index => $match) : ?>
+            <!-- CICLO SU ARRAY PRICIPALE -->
+            <?php foreach ($array_matches as $index => $match) : ?>
             <hr>
-                <!-- CICLO SU ARRAY SECONDARIO E RECUPERO LE CHIAVI -->
-                <?php foreach($match as $key => $team) : ?>
-                    <li class="my-3">
-                        <!-- STAMPO LE CHIAVI -->
-                        <h4 class="text-primary"><?= $key ?></h4>
-                        <!-- CICLO PER VEDERE I VALORI DELL'ARRAY SECONDARIO -->
-                        <?php foreach($team as $team_key => $squad) : ?>
-                            <li>
-                                <!-- STAMPO SIA LA CHIAVE CHE IL VALORE -->
-                                <em><?= $team_key ?>: </em>   
-                                <strong><?= $squad ?></strong>
-                            </li>
-                        <?php endforeach ; ?>
-                    </li>
-                <?php endforeach ; ?>
-        <?php endforeach ;?>
+            <!-- CICLO SU ARRAY SECONDARIO E RECUPERO LE CHIAVI -->
+            <?php foreach ($match as $key => $team) : ?>
+            <li class="my-3">
+                <!-- STAMPO LE CHIAVI (CASA/FUORICASA) -->
+                <h4 class="text-primary"><?= $key ?></h4>
+                <!-- CICLO PER VEDERE I VALORI DELL'ARRAY SECONDARIO -->
+                <?php foreach ($team as $team_key => $squad) : ?>
+            <li>
+                <!-- STAMPO SIA LA CHIAVE CHE IL VALORE -->
+                <em><?= $team_key ?>: </em>
+                <strong><?= $squad ?></strong>
+            </li>
+            <?php endforeach; ?>
+            </li>
+            <?php endforeach; ?>
+            <?php endforeach; ?>
         </ul>
     </div>
 </body>
+
 </html>
--- snack2/php/script.php
+++ snack2/php/script.php
@@ -1,8 +1,13 @@
-<?php 
+<?php
+
+
+/* RECUPERO VALORI INSERITI DALL'UTENTE */
 $name = $_GET['name'] ?? '';
 $email = $_GET['email'] ?? '';
 $age = $_GET['age'] ?? '';
 
+
+/* CONDIZIONE DI VALIDAZIONE DI ACCESSO */
 if (strlen($name) <= 3  || (!str_contains($email, '@')) || (!str_contains($email, '.')) || (!is_numeric($age))) {
     $color = 'text-red';
     $message = "Accesso negato!";
@@ -16,16 +21,19 @@ if (strlen($name) <= 3  || (!str_contains($email, '@')) || (!str_contains($email
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SNACK2</title>
     <link rel="stylesheet" href="./css/style.css">
 </head>
+
 <body>
     <div>
+        <!-- STAMPO IL MESSAGGIO -->
         <h1 class="<?=$color?>"> <?= $message?></h1>
     </div>
 </body>
-</html>
 
+</html>
